f96d1c75d6acc7d03b3d51413db990b4a4a079cc
nam2626 <nam2626@gmail.com>
2021-09-10 19:59:51 +0900

    변수

---
+++ Step02/src/PrintTest.java
@@ -0,0 +1,20 @@
+
+public class PrintTest {
+	/*  
+	 * 주석 : 파일에 삽입되는 코드이지만, 실행이 되지 않는 코드
+	  		 현재 코드에 설명을 쓰는 용도
+	   // : 단일행 주석
+	 */
+	
+	public static void main(String[] args) {
+		System.out.print("출력할\n내용1");
+		/*
+		System.out.println("출력할 내용2");
+		System.out.println("출력할 내용3");
+		System.out.println();//내용 출력이 없으면 줄바꿈만 함
+		*/
+		//System.out.println("출력할 내용4");
+		
+	}
+
+}
