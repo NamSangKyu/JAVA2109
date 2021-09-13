a6681a234b71b3e87b0839e2af16cc03a38c8ce8
nam2626 <nam2626@gmail.com>
2021-09-13 21:43:51 +0900

    데이터 형변환

---
+++ Step03/src/CastingMain.java
@@ -0,0 +1,20 @@
+
+public class CastingMain {
+	/*
+	 * 데이터 형변환
+	 * 		int	   ====> double
+	 * 		double ====> int
+	 * 		타입을 바꾸는 행위를 형변환
+	 * 강제로 형변환
+	 * 자동으로 형변환
+	 */
+	public static void main(String[] args) {
+		int r = 3;
+		double area = 0.0;
+		
+		area = 3 * 3 * 3.1415;
+		
+		System.out.println(area);
+	}
+
+}
