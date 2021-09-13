88e3269469c7815024c9cfa2ffec2588723694cb
nam2626 <nam2626@gmail.com>
2021-09-13 20:03:32 +0900

    변수타입 - char

---
+++ Step03/src/CharType.java
@@ -0,0 +1,21 @@
+
+public class CharType {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		int n1 = ch1; //문자코드 값을 숫자로 형변환
+		System.out.println(n1);
+		ch1++;
+		n1 = ch1;
+		System.out.println(ch1);
+		System.out.println(n1);
+		
+		char ch2 = 'Z';
+		int n2 = ch2;
+		System.out.println(ch2);
+		System.out.println(n2);
+		
+	}
+
+}
