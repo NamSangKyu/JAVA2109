83bcb87a50f6328a326f9e98bef3ec76b36781a0
nam2626 <nam2626@gmail.com>
2021-09-15 20:29:41 +0900

    반복문

---
+++ Step04/src/WhileTest1.java
@@ -0,0 +1,13 @@
+
+public class WhileTest1 {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i != 10) {//(조건식) 안에 있는 조건식이 true일 동안 실행
+			System.out.println("HelloWorld");
+			i++;
+		}
+		
+	}
+
+}
---
+++ Step04/src/WhileTest2.java
@@ -0,0 +1,11 @@
+
+public class WhileTest2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 	숫자 1~100까지 숫자를 출력
+		 * 
+		 */
+	}
+
+}
