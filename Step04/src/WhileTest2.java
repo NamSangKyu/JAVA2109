fd442560d314e0caec2e038652eb558a484bf7b4
nam2626 <nam2626@gmail.com>
2021-09-15 20:37:11 +0900

    반복문

--- Step04/src/WhileTest2.java
+++ Step04/src/WhileTest2.java
@@ -6,6 +6,14 @@ public class WhileTest2 {
 		 * 	숫자 1~100까지 숫자를 출력
 		 * 
 		 */
+		//정수형 변수 1개 - 초기값 1
+		int n = 1;
+		//반복문 -  100일때 종료
+		while(n <= 100) {
+			//	1~100까지 숫자 출력
+			System.out.println(n);
+			n++;
+		}
 	}
 
 }
