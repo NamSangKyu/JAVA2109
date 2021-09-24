f7c4799a879f7bcc1dc1cbd3923fe56f79a39ca6
nam2626 <nam2626@gmail.com>
2021-09-24 19:34:47 +0900

    for 반복문

---
+++ Step05/src/ForTest1.java
@@ -0,0 +1,12 @@
+
+public class ForTest1 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i=0;i<10;i++) {
+			System.out.println("Hello World");
+		}
+	}
+
+}
---
+++ Step05/src/ForTest2.java
@@ -0,0 +1,13 @@
+
+public class ForTest2 {
+
+	public static void main(String[] args) {
+		int i=0;
+		//초기화, 조건식, 증감식이 필요없으면 그냥 공간을 비워두면 됨
+		for(	;i<10;	) {
+			System.out.println("Hello World");
+			i++;
+		}
+	}
+
+}
