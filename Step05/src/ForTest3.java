c4cc6704118340c349b7fb310461e1264dedc5aa
nam2626 <nam2626@gmail.com>
2021-09-24 19:46:15 +0900

    for 반복문

---
+++ Step05/src/ForTest3.java
@@ -0,0 +1,15 @@
+
+public class ForTest3 {
+
+	public static void main(String[] args) {
+		/*
+		 * 	숫자 1~100까지 숫자를 출력
+		 */
+		//초기화 부분에 변수를 선언하면은 for문에서만 해당변수를 사용할 수 있다.
+		//for문이 끝나면 해당 변수는 사라짐
+		for(int i=1;i<=100;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
