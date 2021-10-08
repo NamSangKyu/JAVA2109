e48d5fb68d08cd0dffeb37018a2516ac723163b9
nam2626 <nam2626@gmail.com>
2021-10-08 21:27:40 +0900

    클래스 작성

--- Step10/src/TV.java
+++ Step10/src/TV.java
@@ -1,10 +1,16 @@
 /*
  *  TV 클래스
- *  	데이터
+ *  	필드
  *  		채널 - int --> 1 ~ 356
  *  		음량 - int --> 0 ~ 40
  *  		전원 - boolean --> On - true, Off - false
  *  		음소거 - boolean --> On - true, Off - false
+ *  	메서드
+ *  		채널Up : 채널값 1씩 증가
+ *  		채널Down : 채널값을 1씩 감소
+ *  		전원OnOff : true 전원 On, false 전원 Off
+ *  		음량Up : 음량 1씩 증가 -> 최대값
+ *  		음량Down : 음량 1씩 감소 -> 최소값 
  */
 public class TV {
 	private int ch;
@@ -23,6 +29,39 @@ public class TV {
 		mute = false;
 	}
 
+	public void chUp() {
+		ch++;//채널이 최대값? 
+		if(ch > MAX_CH)
+			ch = MIN_CH;
+		System.out.println("현재 채널 : "+ch);
+	}
+	
+	public void chDown() {
+		ch--;//채널이 최소값?
+		if(ch < MIN_CH)
+			ch = MAX_CH;
+		System.out.println("현재 채널 : "+ch);
+	}
+	
+	public void powerOnOff() {
+		power = !power;
+		if(power)
+			System.out.println("TV 전원 On");
+		else
+			System.out.println("TV 전원 Off");
+	}
+	
+	public void volUp() {
+		if(vol < MAX_VOL)
+			vol++;
+		System.out.println("현재 음량 : "+vol);
+	}
+	public void volDown() {
+		if(vol > MIN_VOL)
+			vol--;
+		System.out.println("현재 음량 : "+vol);
+	}
+	
 	public int getCh() {
 		return ch;
 	}
