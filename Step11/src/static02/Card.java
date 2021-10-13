85c39536d1c4a0785d3a40e1d6b44e7655dc489b
nam2626 <nam2626@gmail.com>
2021-10-13 20:12:55 +0900

    static 예제

--- Step11/src/static02/Card.java
+++ Step11/src/static02/Card.java
@@ -1,12 +1,14 @@
 package static02;
 
 public class Card {
-	private int cardNumber = 1000;
+	public static final long CARD_VERSION = 1L;
+	private static int cardSequence = 1000;
+	private int cardNumber;
 	private String name;
 
 	public Card(String name) {
 		this.name = name;
-		cardNumber++;
+		cardNumber = cardSequence++;
 	}
 	
 	public void printCardInfo() {
--- Step11/src/static02/CardMain.java
+++ Step11/src/static02/CardMain.java
@@ -7,7 +7,7 @@ public class CardMain {
 		Card c2 =  new Card("홍길동");
 		Card c3 =  new Card("이영수");
 		Card c4 =  new Card("박영희");
-		
+		System.out.println("카드프로그램 버전 : "+ Card.CARD_VERSION);
 		c1.printCardInfo();
 		c2.printCardInfo();
 		c3.printCardInfo();
