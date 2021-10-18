ec41810a306d7a13c368be214bcd3b18e5000c1d
nam2626 <nam2626@gmail.com>
2021-10-18 19:49:23 +0900

    추상클래스 예제

--- Step13/src/abstract_power/Boiler.java
+++ Step13/src/abstract_power/Boiler.java
@@ -1,5 +1,21 @@
 package abstract_power;
 
 public class Boiler {
+	private PowerOnOff onOff;
 
+	public Boiler(PowerOnOff onOff) {
+		this.onOff = onOff;
+	}
+
+	public PowerOnOff getOnOff() {
+		return onOff;
+	}
+
+	public void setOnOff(PowerOnOff onOff) {
+		this.onOff = onOff;
+	}
+	
+	public void powerOnOff() {
+		onOff.powerOnOff();
+	}
 }
