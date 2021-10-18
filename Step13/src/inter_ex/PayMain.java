7df8739f17fd79a424d060d3ea6508e55f070260
nam2626 <nam2626@gmail.com>
2021-10-18 21:48:29 +0900

    인터페이스 예제

--- Step13/src/inter_ex/PayMain.java
+++ Step13/src/inter_ex/PayMain.java
@@ -3,7 +3,16 @@ package inter_ex;
 public class PayMain {
 
 	public static void main(String[] args) {
-		
+		Pay[] arr = new Pay[3];
+		arr[0] = new Cooker("홍길동");
+		arr[1] = new Player("김철수",10);
+		arr[2] = new Employee("이영희", "대리");
+		pay(arr,1000);
 	}
-
+	
+	public static void pay(Pay[] arr, int money) {
+		for(int i=0;i < arr.length;i++) {
+			arr[i].pay(money);
+		}
+	}
 }
