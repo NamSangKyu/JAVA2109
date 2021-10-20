7b51ea535f6036f16bc82142b2bf27cba404958f
nam2626 <nam2626@gmail.com>
2021-10-20 19:41:13 +0900

    인터페이스 예제

--- Step14/src/inter_inherit/B.java
+++ Step14/src/inter_inherit/B.java
@@ -2,4 +2,7 @@ package inter_inherit;
 
 public interface B extends A{
 	public void methodB();
+	default void method1() {
+		System.out.println("B method1");
+	}
 }
--- Step14/src/inter_inherit/C.java
+++ Step14/src/inter_inherit/C.java
@@ -2,4 +2,7 @@ package inter_inherit;
 
 public interface C extends A{
 	public void methodC();
+	default void method1() {
+		System.out.println("C method1");
+	}
 }
