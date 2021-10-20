8af87a43f27fdd9037feedbe1451b105ea4a1a69
nam2626 <nam2626@gmail.com>
2021-10-20 19:15:44 +0900

    인터페이스 예제

---
+++ Step14/src/inter/PowerOnOff.java
@@ -0,0 +1,15 @@
+package inter;
+
+public interface PowerOnOff {
+	//상수 선언시 public static final 자동으로 붙음
+	public static final boolean POWER_ON = true;
+	public static final boolean POWER_OFF = false;
+	
+	//자동으로 추상메서드 선언됨
+	public abstract void powerOnOff();
+	
+	//jdk 8버전부터 추가됨
+	default void powerOn() {
+		System.out.println("전원 On");
+	}
+}
