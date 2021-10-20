b675158bae2c1905602912282f5e982743ac44b5
nam2626 <nam2626@gmail.com>
2021-10-20 21:41:04 +0900

    Generic 예제

--- Step14/src/generic/BoxMain.java
+++ Step14/src/generic/BoxMain.java
@@ -5,10 +5,19 @@ public class BoxMain {
 	public static void main(String[] args) {
 		Box box = new Box();
 		box.setObj(new Apple());
-		box.setObj(new Orange());
+		//box.setObj(new Orange());
 		Apple apple = (Apple) box.getObj();
 		apple.printApple();
 		
+		
+		BoxGeneric<Apple> boxApple = new BoxGeneric<Apple>();
+		boxApple.setObj(new Apple());
+		apple = boxApple.getObj();
+		apple.printApple();
+		BoxGeneric<Orange> boxOrage = new BoxGeneric<Orange>();
+		boxOrage.setObj(new Orange());
+		Orange orange = boxOrage.getObj();
+		orange.printOrange();
 	}
 
 }
---
+++ Step14/src/generic_extends/ImportMouse.java
@@ -0,0 +1,19 @@
+package generic_extends;
+
+public class ImportMouse <T extends Mouse>{
+	private T mouse;
+
+	public ImportMouse(T mouse) {
+		this.mouse = mouse;
+	}
+
+	public void click(int n) {
+		mouse.click(n);
+	}
+	
+	public void wheel() {
+		mouse.wheel();
+	}
+	
+
+}
