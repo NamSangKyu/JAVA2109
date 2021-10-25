cee579873c5a649fabfef7376ca491f8303e50db
nam2626 <nam2626@gmail.com>
2021-10-25 21:26:39 +0900

    ArrayList 예제

--- Step15/src/list/ListExample.java
+++ Step15/src/list/ListExample.java
@@ -29,7 +29,24 @@ public class ListExample {
 		list.remove("java");
 		System.out.println(list.toString());
 		
-		//
+		//저장하고 있는 데이터 개수
+		System.out.println(list.size());
+		
+		//해당 데이터가 리스트에 있는지?
+		System.out.println(list.contains("jsp"));
+		System.out.println(list.contains("jsp1"));
+		
+		//리스트 내용 비우기
+//		list.clear();
+//		System.out.println(list);
+		//리스트 내용이 비었냐? 비었으면 true, 안비었으면 false
+		System.out.println(list.isEmpty());
+
+		//리스트를 원하는 인덱스에 추가
+		list.add(1, "java");
+		System.out.println(list);
+		
+		//리스트에 있는 모든 객체 꺼내기 - 1
 		
 	}
 
