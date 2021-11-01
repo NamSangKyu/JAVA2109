56760f91dd2c15568402839b4c62c28f2d84f7df
nam2626 <nam2626@gmail.com>
2021-11-01 21:44:14 +0900

    스레드 라이프 사이클

--- Step18/src/wait/JobSeeker.java
+++ Step18/src/wait/JobSeeker.java
@@ -12,9 +12,7 @@ public class JobSeeker extends Thread {
 			while (true) {
 				System.out.println(getName() + "가 직업 정보 수신을 대기 중입니다.");
 				String msg = JobInfo.getInstance().getJobInfo();
-				if(msg == null)
-					msg = "이미 다른 누군가가 수신해 갔습니다.";
-				else {
+				if(msg != null){
 					System.out.println(msg + "는 " + getName()+"님이 수신하였습니다.");
 					System.out.println(getName() + " 구직자 지원 종료");
 					return;
